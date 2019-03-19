ae44576856c64b7b7638ab6c76f0893033acab88
Benny Zlotnik <uber442@gmail.com>
2019-03-19 10:32:12 +0200

    Initial dashboard

---
+++ src/components/Menu.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      left
+      app>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>fa-server</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Hosts</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+        </v-layout>
+      </v-container>
+    </v-content>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  poo
+  <app-menu></app-menu>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  import Menu from './../components/Menu';
+  export default {
+    components: {
+      appMenu: Menu
+    },
+    data() {
+      return {
+      }
+    }
   }
-}
 </script>
